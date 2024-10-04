d95120628d05b141aea75ff518d2fc1032da4c80
Christba03 <christba455@gmail.com>
2024-10-03 20:07:22 -0600

    feat: add bubble sort algorithm
    se implemento el nuevo metodo de sorting, bubble

--- java/Java_Generic_Project/src/App.java
+++ java/Java_Generic_Project/src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int[] array = { 5, 3, 8, 4, 2 };
+        System.out.println("Array before Bubble Sort:");
+        printArray(array);
+
+        SortingAlgorithms.bubbleSort(array);
+
+        System.out.println("Array after Bubble Sort:");
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
