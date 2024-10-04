d7d6f995da8ac86842ac7c0cef05596bd3513264
Christba03 <christba455@gmail.com>
2024-10-03 20:12:32 -0600

    feat: add insertion sort algorithm

--- java/Java_Generic_Project/src/SortingAlgorithms.java
+++ java/Java_Generic_Project/src/SortingAlgorithms.java
@@ -28,6 +28,19 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
     private static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
